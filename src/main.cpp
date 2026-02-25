6ca9c3a67a2e05d5c8eb68ac992098602dd64c43
Fernando Avalos <fernandoadev@protonmail.com>
2026-02-25 13:47:10 -0500

    Fix render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Config.hpp>
+#include <iostream>
 
 #include "Utils.hpp"
 #include "Constants.hpp"
@@ -9,13 +10,13 @@ void setup(World& world) {
 	Body b1;
 	b1.r.x = 0.0;
 	b1.r.y = 0.0;
-	b1.mass = 1.0;
+	b1.mass = 200.0;
 	b1.density = 1.0;
 
 	Body b2;
 	b2.r.x = 5.0;
 	b2.r.y = 0.0;
-	b2.mass = 1.0;
+	b2.mass = 100.0;
 	b2.density = 1.0;
 
 	world.add_body(b1);
@@ -23,12 +24,12 @@ void setup(World& world) {
 }
 
 void draw_bodies(World& world, sf::RenderWindow& window) {
-	std::vector<Body>::iterator it;
 	std::vector<Body> bodies = world.bodies;
 
-	for (it = bodies.begin(); it != bodies.end(); ++it) {
-		sf::CircleShape shape(it->calc_radius());
-		shape.setFillColor(sf::Color::Green);
+	for (auto& element : bodies) {
+		double radius = element.calc_radius();
+		sf::CircleShape shape(radius);
+		shape.setFillColor(sf::Color::Red);
 		window.draw(shape);
 	}
 }
