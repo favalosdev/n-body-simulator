4332f49b0b060941369af5e312d65fa7a6f82dd8
Fernando Avalos <fernandoadev@protonmail.com>
2026-03-02 07:05:55 -0500

    Fix coordinate system

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ void setup(World& world)
 	b1.r.y = 0.0;
 	b1.mass = 200.0;
 	b1.density = 0.001;
-	b1.colour = get_random_colour(); 
+	b1.colour = sf::Color::Yellow;
 	world.add_body(b1);
 
 	Body b2;
@@ -27,7 +27,7 @@ void setup(World& world)
 	b2.r.y = 0.0;
 	b2.mass = 200.0;
 	b2.density = 0.001;
-	b2.colour = get_random_colour();
+	b2.colour = sf::Color::Blue;
 	world.add_body(b2);
 
 	Body b3;
@@ -35,16 +35,9 @@ void setup(World& world)
 	b3.r.y = 200.0;
 	b3.mass = 200.0;
 	b3.density = 0.001;
-	b3.colour = get_random_colour();
+	b3.colour = sf::Color::Red;
 	world.add_body(b3);
 }
-
-/*
-Stuff that's pending:
-1. Modify the main window's coordinate system
-2. Place bodies according to their actual coordinates
-3. Simulate small steps to see if everything is working
-*/
 
 void draw_bodies(World& world, sf::RenderWindow& window)
 {
@@ -52,7 +45,7 @@ void draw_bodies(World& world, sf::RenderWindow& window)
 		float radius = b.calc_radius();
 		sf::CircleShape shape(radius);
 		shape.setFillColor(b.colour);
-		sf::Vector2f pos = { b.r.x + WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - b.r.y };
+		sf::Vector2f pos = { b.r.x + WINDOW_WIDTH / 2 - radius, WINDOW_HEIGHT / 2 - b.r.y - radius };
 		shape.setPosition(pos);
 		window.draw(shape);
 	}
